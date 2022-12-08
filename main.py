68870609acfa84da3aa3bc6725e4c4502f6a7770
nono-010000010100100101010100 <120077374+nono-010000010100100101010100@users.noreply.github.com>
2022-12-08 15:41:25 +0900

    add: Yamamotoの日記を追加

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from diaries.DiarySample import DiarySample
+from diaries.YamamotoDiary import YamamotoDiary
 
-diaries = [DiarySample(), ]
+diaries = [DiarySample(), YamamotoDiary(),]
 
 for d in diaries:
     print("--------------------")
